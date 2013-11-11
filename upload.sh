f983e3cc41a5c20a7abc8315bef8c1480c6d446a
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2013-11-11 15:18:24 +0100

    update upload script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+git remote add github git@github.com:openmeeg/openmeeg.github.io.git
 touch build/html/.nojekyll
-ghp-import -p build/html -r origin
+ghp-import -p build/html -r origin -b master
